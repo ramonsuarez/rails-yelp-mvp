4b45e13f898607e41d7ee2e1c81468c9a73c5504
Ramon Suarez <ramon@betacowork.com>
2016-11-23 16:58:48 +0100

    Controllers done but error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,29 +1,42 @@
 class RestaurantsController < ApplicationController
-
-  def index
+before_action :set_restaurant, only: [:index, :show, :new, :create]
+  def index         # GET /restaurants
+    @restaurants = Restaurant.all
   end
 
-  def show
+  def show          # GET /restaurants/:id
   end
 
-  def new
+  def new           # GET /restaurants/new
+    @restaurant = Restaurant.new
   end
 
-  def create
+  def create        # POST /restaurants
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
   end
 
-  def edit
+  def edit          # GET /restaurants/:id/edit
   end
 
-  def update
+  def update        # PATCH /restaurants/:id
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
-  def destroy
+  def destroy       # DELETE /restaurants/:id
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
 
+  def restaurant_params
+    params.require(:restaurant).permit(:description, :due_date, :priority)
+  end
 
-
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+def show
+
+def new
+  @review = Review.new
+end
+
+def create
+    @review = @restaurant.reviews.build(review_params)
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+
+private
+
+def review_params
+  params.require(:review).permit(:description, :due_date, :priority)
+end
+
+def set_review
+  @review = Review.find(params[:id])
+end
+
 end
